2facd896ad7d230470dd8e8b2a5176693b313f8a
Tanvi <viramgamatanvi@gmail.com>
2025-10-27 16:53:45 +0530

    python

---
+++ practice.py
@@ -0,0 +1,128 @@
+# print("Tanvi")
+# print("Hii")
+# print(34)
+# print(34+45)
+# print("Hello")
+# print("hii",45,'hello world')
+
+
+# print("Viramgama", end=" ")
+# print("Tanvi")
+
+
+# a=2
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a%b)
+# print(a*b)
+# print(a/b)
+
+
+
+
+# name=input("Enter your name : ")
+# lastname=input("Enter your last name : ")
+
+# print("The Student full name is",name,lastname)
+# print("The Student full name is",name+ " " +lastname)
+
+
+
+
+# call=input()
+# print(call)
+
+
+
+# call=input("Enter your call : ")
+# print(call)
+
+
+# print("One","two","three",sep="-")
+
+
+'''
+This is a multi-line comment
+using triple single quotes.
+It can span multiple lines.
+'''
+
+
+
+# data=(12)
+# print(data)
+
+
+
+# data="Tanvi"
+# print(data)
+
+
+# data='python'
+# print(data)
+
+# data="python"
+# print(data)
+
+# data='''python'''
+# print(data)
+
+
+# data="python"
+# del data
+# print(data)
+
+
+# a=2
+# b=4
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+# print(a//b)
+
+
+
+# a=2
+# b=2
+
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+# x=3
+# x+=5
+# x-=5
+# x*=5
+# x/=5
+# x%=5
+# x//=5
+# x**=5
+
+# print(x)
+
+
+
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+
+# print(a is b)  
+# print(a is c)
+
+
+
+# result=2+3-4*2/2
+# print(result)
+
+# course="python"
+# print(id(course))
+# print(type(course))
