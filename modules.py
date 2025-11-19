dd3a67b43a069b7bf86efc0941eb3ca20816ef9c
Tanvi <viramgamatanvi@gmail.com>
2025-11-19 15:44:41 +0530

    hh:

---
+++ modules.py
@@ -0,0 +1,78 @@
+# Module and it's Types
+
+
+# Types of Modules in Python
+
+
+# Built-in Modules
+# User-defined Modules
+# Third-party Modules
+
+
+# Built-in Modules
+
+# import math
+
+# print(math.sqrt(25))   
+# print(math.factorial(5))  
+# print(math.pi)
+
+
+
+
+#  User-Defined Modules
+
+
+#  Step 1: Create a file module.py
+
+
+# def greet(name):
+#     return f"Hello, {name}!"
+
+# pi = 3.14159
+
+
+
+# Step 2: Import & Use in Another File (main.py)
+
+
+# import modules
+
+# print(modules.greet("Alice"))  
+# print(modules.pi)
+
+
+
+
+# Importing Specific Functions or Variables
+
+
+# from math import sqrt, pi
+
+
+# print(sqrt(16)) 
+# print(pi)
+
+
+
+# Third-Party Modules
+
+
+# pip install requests
+
+# import requests
+
+# response = requests.get("https://www.google.com")
+# print(response.status_code)
+
+
+
+
+
+# datetime module
+
+
+# Importing the datetime Module
+
+# import datetime
+
